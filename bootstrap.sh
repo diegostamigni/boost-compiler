4bc85b75cc61788bec49b737229e6566e1926e97
Diego Stamigni <diego.stamigni@gmail.com>
2014-10-06 14:32:15 +0200

    info added  on boost-compiler bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+##
+#### Diego Stamigni
+#### diego.stamigni@liveboxcloud.com
+
+# Available libs that can be built are listed below:
+# - chrono
+# - context
+# - filesystem
+# - graph_parallel
+# - iostreams
+# - locale
+# - mpi
+# - program_options
+# - python
+# - regex
+# - serialization
+# - signals
+# - system
+# - thread
+# - timer
+# - wave
+# - date_time
+# - graph
+# - math
+# - random
+# - test
+# - exception
 
 : ${BOOST_LIBS:="system signals filesystem"}
-
