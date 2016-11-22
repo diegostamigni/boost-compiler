cc8cf1a903dbf5b4aa8d760000925a0a83079e20
Diego Stamigni <diego.stamigni@gmail.com>
2016-11-22 10:20:21 +0000

    useless libs removed from def compilation

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,4 +35,4 @@
 # - type_erasure
 # - wave
 
-: ${BOOST_LIBS:="atomic chrono container context coroutine coroutine2 date_time exception fiber filesystem graph graph_parallel iostreams locale log math metaparse mpi program_options python random regex serialization signals system test thread timer type_erasure wave"}
+: ${BOOST_LIBS:="system thread regex log"}
